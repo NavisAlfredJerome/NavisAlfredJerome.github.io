9d13772d5d19dbdba6d530e3f2ad65abedaba9d2
francis <francisantontyraj1@gmail.com>
2024-02-04 14:58:43 -0500

    new json data's

--- src/script.js
+++ src/script.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
+    southIndia();
+    east();
+    west();
+    centralndia();
+    northIndia();
+});
+
+function southIndia(){
     fetch('resources/southIndiaData.json')
     .then(response => response.json())
     .then(data =>{
-        const tableBody = document.getElementById('tableBody');
+        const tableBody = document.getElementById('southIndiaDataTableBody');
 
         data.forEach(item =>{
             const row = document.createElement('tr');
@@ -19,7 +27,99 @@ document.addEventListener('DOMContentLoaded', function(){
         });
     })
     .catch(error => console.error('Error fetching data:', error));
-});
+}
+
+function east(){
+    fetch('resources/eastData.json')
+    .then(response => response.json())
+    .then(data =>{
+        const tableBody = document.getElementById('eastDataTableBody');
+
+        data.forEach(item =>{
+            const row = document.createElement('tr');
+
+            row.innerHTML = `
+            <td>${item.state}</td>
+            <td>${item.totalSeats}</td>
+            <td>${item.indiaAlliance}</td>
+            <td>${item.nda}</td>
+            <td>${item.others}</td>
+            `;
+
+            tableBody.appendChild(row);
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+}
+
+function west(){
+    fetch('resources/westData.json')
+    .then(response => response.json())
+    .then(data =>{
+        const tableBody = document.getElementById('westDataTableBody');
+
+        data.forEach(item =>{
+            const row = document.createElement('tr');
+
+            row.innerHTML = `
+            <td>${item.state}</td>
+            <td>${item.totalSeats}</td>
+            <td>${item.indiaAlliance}</td>
+            <td>${item.nda}</td>
+            <td>${item.others}</td>
+            `;
+
+            tableBody.appendChild(row);
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+}
+
+function centralndia(){
+    fetch('resources/centralIndiaData.json')
+    .then(response => response.json())
+    .then(data =>{
+        const tableBody = document.getElementById('centralIndiaDataTableBody');
+
+        data.forEach(item =>{
+            const row = document.createElement('tr');
+
+            row.innerHTML = `
+            <td>${item.state}</td>
+            <td>${item.totalSeats}</td>
+            <td>${item.indiaAlliance}</td>
+            <td>${item.nda}</td>
+            <td>${item.others}</td>
+            `;
+
+            tableBody.appendChild(row);
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+}
+
+function northIndia(){
+    fetch('resources/northIndiaData.json')
+    .then(response => response.json())
+    .then(data =>{
+        const tableBody = document.getElementById('northIndiaDataTableBody');
+
+        data.forEach(item =>{
+            const row = document.createElement('tr');
+
+            row.innerHTML = `
+            <td>${item.state}</td>
+            <td>${item.totalSeats}</td>
+            <td>${item.indiaAlliance}</td>
+            <td>${item.nda}</td>
+            <td>${item.others}</td>
+            `;
+
+            tableBody.appendChild(row);
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+}
 
 function displayContent(contentId){
     var contentElements=document.getElementsByClassName('content');
